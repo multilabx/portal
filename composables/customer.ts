0cd7d45bdfcbf36e42f7ee406924a4f02176b45b
Zensos <rockemaner@gmail.com>
2022-10-20 01:55:03 +0700

    😎Re-image

--- composables/customer.ts
+++ composables/customer.ts
@@ -2,29 +2,29 @@ export const useCustomer = () => {
   const customers: {
     image: string;
   }[] = [
-    {
-      image: "https://easyfarmshop.in.th/logo.png",
-    },
-    {
-      image: "https://saladbux.wtf/_nuxt/img/logo.6966fb4.png",
-    },
-    {
-      image: "https://micahshops.com/logo.png",
-    },
-    {
-      image: "https://bloxcodesth.com/logo.png",
-    },
-    {
-      image: "https://onishop.me/logoSqure.png",
-    },
-    {
-      image:
-        "https://media.discordapp.net/attachments/872102608909795449/944274854121701416/logo.png",
-    },
-    {
-      image:
-        "https://shopfarm.in.th/_next/image?url=https%3A%2F%2Fmedia.discordapp.net%2Fattachments%2F985170346338562080%2F987363981536677888%2FSHOPFARMLOGO4.png%3Fwidth%3D618%26height%3D618&w=96&q=75",
-    },
-  ];
+      {
+        image: "https://easyfarmshop.in.th/logo.png",
+      },
+      {
+        image: "https://saladbux.wtf/logo.png",
+      },
+      {
+        image: "https://micahshops.com/logo.png",
+      },
+      {
+        image: "https://bloxcodesth.com/logo.png",
+      },
+      {
+        image: "https://onishop.me/logoSqure.png",
+      },
+      {
+        image:
+          "https://media.discordapp.net/attachments/872102608909795449/944274854121701416/logo.png",
+      },
+      {
+        image:
+          "https://shopfarm.in.th/_next/image?url=https%3A%2F%2Fmedia.discordapp.net%2Fattachments%2F985170346338562080%2F987363981536677888%2FSHOPFARMLOGO4.png%3Fwidth%3D618%26height%3D618&w=96&q=75",
+      },
+    ];
   return customers;
 };
