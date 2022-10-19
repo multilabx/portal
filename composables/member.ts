5eee1515ad9a527b1047d3996f0b2ae3f2b23997
Zensos <rockemaner@gmail.com>
2022-10-20 01:50:58 +0700

    ⚡ New Image

--- composables/member.ts
+++ composables/member.ts
@@ -5,112 +5,112 @@ export const useMember = () => {
     description: string;
     skill: string;
   }[] = [
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-6/289533777_720425255843997_845496258667553772_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHrHgJTgKRZ6eJ35NJ961SpZ_mbrzQuyUhn-ZuvNC7JSLNxdN3usHktqgw2ecISkeplg7PaBowuaOv45CKEBXPl&_nc_ohc=5PQipj7Ay8cAX_Cr-hb&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT9r-7kSZBtCkrfWbC0YHjDeawsjZfxNTqhEhdr63ZSupw&oe=634D7B8C",
-      nickName: "BossNz",
-      description: "Co-Founder",
-      skill: "devops engineer",
-    },
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-1/311216519_1354646091732935_4961676488751155954_n.jpg?stp=cp6_dst-jpg_p480x480&_nc_cat=111&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHcEUVXyvuF8Gs0ubszO2Gyz-arwDRuZEjP5qvANG5kSDQQR9kf9zSZ0luiot8Fjby9R5dDrx8BVBtfABTNOx5R&_nc_ohc=7dmx0uHHe34AX_F0Nrg&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT-DHYMh4yHYBORGYYxcM4qq_T2OVAUN9EhZvylKUdMaog&oe=634D7BB6",
-      nickName: "Zensos",
-      description: "Co-Founder",
-      skill: "Backend Develop",
-    },
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-1/212612163_189423113117957_3007422640843340802_n.jpg?stp=c169.1.427.428a_dst-jpg_p720x720&_nc_cat=109&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHB_Yn53pd4648Tkoa5RNNEp8bvnbciSX6nxu-dtyJJfk3ZUzdjzRevG-ZR24Rl89be_ylsCFDioxO0t2ml2JXV&_nc_ohc=f6AFlZUPKSAAX-7R58a&_nc_oc=AQltI1SYxPM5mJGglpAMa9sEd-95FiOs3ha5LSm1AVps60ENJo2MyDQY7QlrNU2Vejs&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT87rKUHvd29TuIoHvV5_LNgBtTa4Ch6NdP8Fc-MOZ6Iaw&oe=634D8601",
-      nickName: "CheLos",
-      description: "Co-Founder",
-      skill: "devops engineer",
-    },
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-1/296842807_719315962442705_4173876293335934358_n.jpg?stp=dst-jpg_s480x480&_nc_cat=102&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHT_tJvHWqPYoJAUk4P8ohPvNDM7cHOU7C80Mztwc5TsNCFsNowhazNC0ysdyy0WaWr0MqBhPnEcGDzkZOZn55o&_nc_ohc=2gEbjZ_4VCwAX8p2QQQ&tn=DC-dcTH4wITef9jj&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT8KOYsYgD6-VyxuOoRO_zzXvOGRbbyKKiTSPKFVo9vL-A&oe=634E67BE",
-      nickName: "NackZa",
-      description: "Superman",
-      skill: "hackerman",
-    },
+      {
+        image:
+          "https://media.discordapp.net/attachments/872102608909795449/1032357736123469874/unknown.png?size=1024",
+        nickName: "BossNz",
+        description: "Co-Founder",
+        skill: "devops engineer",
+      },
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/502056862674452481/abc19c0a32077b5beea10b0d331d0dac.webp?size=1024",
+        nickName: "Zensos",
+        description: "Co-Founder",
+        skill: "Backend Develop",
+      },
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/722467073762000966/36f838836eb638cc1c4efc701a28b1ce.webp?size=1024",
+        nickName: "CheLos",
+        description: "Co-Founder",
+        skill: "devops engineer",
+      },
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/373412590136721408/1cefb3dcfcc37769a62865d8f14c968d.webp?size=1024",
+        nickName: "NackZa",
+        description: "Superman",
+        skill: "hackerman",
+      },
 
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-1/308474055_633556914948951_1025926348407840853_n.jpg?stp=dst-jpg_s480x480&_nc_cat=102&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeGua7hWCYfTq8W7N5-eW8q69rGFPJKhm3b2sYU8kqGbdrte6xQcESrFq_AaWW-mduAwxjkArWNhnhcBcuQt4qid&_nc_ohc=c0mDZOO2-l4AX_1p7DD&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT-m5PDJ1lNcDsCH_RGZnEnWpxyO3sQXYoHk4XTRd9P8LQ&oe=634D81EB",
-      nickName: "TeparitZ",
-      description: "Superman",
-      skill: "Fullstack Develop",
-    },
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-1/289996666_561874922047795_8000791828550528358_n.jpg?stp=dst-jpg_p480x480&_nc_cat=101&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHWOrdzNQZ8KlpwsZiEQ7uCLKWH2uuoloQspYfa66iWhDLTGVDnPtMhHx9iBKImSccqGA10wqguvdOC8svCXLGx&_nc_ohc=nAF4WILk14wAX8CoIgO&tn=DC-dcTH4wITef9jj&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT-oG2vQsRsBNkcuAvniAWeM4e6GMcrk87ne-JryM4yTtg&oe=634E7F7C",
-      nickName: "LILDUI",
-      description: "Superman",
-      skill: "Boxing Fivem",
-    },
-    {
-      image:
-        "https://scontent.fbkk4-1.fna.fbcdn.net/v/t39.30808-1/297942244_1096657731264642_2194899047935010365_n.jpg?stp=cp6_dst-jpg_p320x320&_nc_cat=107&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeGR679C9mDE7JZgpn6PAnGgnXloBcCwHAWdeWgFwLAcBQDBsgu2vEENHHllG62_oLXdvc4m1aa0EW79S2G5C6ip&_nc_ohc=KA5XXE7pnxcAX-Mf9QW&_nc_ht=scontent.fbkk4-1.fna&oh=00_AT8-pLM8oW-FfDQL-ZuQiIQqBJagXWO8dk3oRdSI4uLyZA&oe=634E916D",
-      nickName: "beeb",
-      description: "member",
-      skill: "fullstack devlop",
-    },
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-1/271914125_116138157613497_5665598314603198285_n.jpg?stp=dst-jpg_p480x480&_nc_cat=105&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeGjx8Hh8IrBgLRey9JU8OWtRxunqzBN2l1HG6erME3aXRJqnAZlgL4FEgfI5Kk_InzACXBFCWE6U0TR9olPdKR-&_nc_ohc=hBkjK7ra6vsAX8Sy76P&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT_5Dl6vUFJ7Tp1B98Otcay-AA2-T098aY-SG_00HIVWKw&oe=634E2DEA",
-      nickName: "Kimbius",
-      description: "member",
-      skill: "Fullstack Develop",
-    },
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-6/261083783_2943469942581680_7877022239823708752_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_eui2=AeH8SC-m0VEQRaMSxrhcxyNKQCwL-jiBLC5ALAv6OIEsLhj2gJyotpVQt4BjJz_VGf1lRSFjfo5k2Di0DY7DSn-V&_nc_ohc=1rhoHZMYJv0AX9LR7jb&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT-o_edMttnehkphlw5Gz30dHQF2vmB4yBzuZIVlipfwkA&oe=634F4B77",
-      nickName: "MAMMOZ",
-      description: "member",
-      skill: "Fullstack develop",
-    },
-    {
-      image:
-        "https://media.discordapp.net/attachments/943188689952063499/1030488269449875618/unknown.png?width=676&height=676",
-      nickName: "Feezial",
-      description: "member",
-      skill: "Frontend Develop",
-    },
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-1/300586376_180429111111223_6115043378716281364_n.jpg?stp=dst-jpg_p480x480&_nc_cat=102&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHDhaQOgKE1q-E5jJrquV9VCxiIBkCgdSkLGIgGQKB1KX4x2RVQdhurrHt9RLCpTkPj36DdLey2q_73c9Ezzcxl&_nc_ohc=4Q1l8XHOh1MAX8JhNE3&_nc_oc=AQmzQMB4ek0Os-GiYTldI4lKWQCsHVH-QHO31ueP1oDM8d8IHznJOlHC93mjI0nxMYk&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT-gJxpuKTeSgDWV7TggvFxl8LESoEBK6mNNf7Yo5O9pFQ&oe=634F01EF",
-      nickName: "safestabi",
-      description: "member",
-      skill: "Frontend Develop",
-    },
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-1/285213042_1211634606320547_2782561965451326821_n.jpg?stp=dst-jpg_p480x480&_nc_cat=109&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHk0eLYATqMHwtc9H3knQz3MPWYQW-yGPYw9ZhBb7IY9tWV1Zoa1JN_Dnwm5lFaAmTz8MG-4pQanmGRwvJjOcT4&_nc_ohc=0ttJHW4YCLsAX-xJmhf&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT-GH3wlilBQYdnfn3qSZMBy1k302OE3XawzTaioy0WfdA&oe=634F6671",
-      nickName: "OhayoOnii",
-      description: "member",
-      skill: "Fullstack Develop",
-    },
-  ];
+      {
+        image:
+          "https://media.discordapp.net/attachments/875430223695466556/1032360150528446554/unknown.png?size=1024",
+        nickName: "TeparitZ",
+        description: "Superman",
+        skill: "Fullstack Develop",
+      },
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/693514816093945888/56afb6223a9b104eb9ae48f9f5141299.webp?size=1024",
+        nickName: "LILDUI",
+        description: "Superman",
+        skill: "Boxing Fivem",
+      },
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/572663465718710272/0fdbbe0e17b13f363799fe5b04b8923d.webp?size=1024",
+        nickName: "beeb",
+        description: "member",
+        skill: "fullstack devlop",
+      },
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/780633074190123058/2c0014c12c7a1768ebddf3393a980fff.webp?size=1024",
+        nickName: "Kimbius",
+        description: "member",
+        skill: "Fullstack Develop",
+      },
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/328526902279143434/2a2364196177b74b0390a586eb03a027.webp?size=1024",
+        nickName: "MAMMOZ",
+        description: "member",
+        skill: "Fullstack develop",
+      },
+      {
+        image:
+          "https://media.discordapp.net/attachments/943188689952063499/1030488269449875618/unknown.png?size=1024",
+        nickName: "Feezial",
+        description: "member",
+        skill: "Frontend Develop",
+      },
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/787654855719387157/5a1cfb432572e7487faddcb39930105a.webp?size=1024",
+        nickName: "safestabi",
+        description: "member",
+        skill: "Frontend Develop",
+      },
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/930045306261864448/3ac1293446e5790dba50bc18bff5bbea.webp?size=1024",
+        nickName: "OhayoOnii",
+        description: "member",
+        skill: "Fullstack Develop",
+      },
+    ];
   const Special: {
     image: string;
     nickName: string;
     description: string;
     skill: string;
   }[] = [
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-1/275683246_1475623749523244_3262851038714080153_n.jpg?stp=dst-jpg_p480x480&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeFclAhR3u_2BN7DYt5hkqZB7_6CNtyRjBrv_oI23JGMGn0L-9fFGqjm1Bk4Tm0Oy5SGSMb26mnhmEprK1KStXGb&_nc_ohc=2liSS3Z9TwIAX9fuvQR&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT-6wJ8HlE2y7H8w5vWIT3C1JpkiGiD6yF4GsVegII6u0Q&oe=6350A267",
-      nickName: "DeeKit",
-      description: "Special",
-      skill: "Firewall Man",
-    },
-    {
-      image:
-        "https://scontent.fbkk17-1.fna.fbcdn.net/v/t39.30808-1/294285565_731979478106137_7756374426926458703_n.jpg?stp=dst-jpg_s480x480&_nc_cat=103&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeG0dg7JhpDG4aTUp5I3ZONzReKMgRaobh1F4oyBFqhuHVvJyhThVBbFp38txrxO9C5FiwBGB6CW_H77aPsAd6xI&_nc_ohc=O6FqYBMoNKMAX-P-QFC&_nc_ht=scontent.fbkk17-1.fna&oh=00_AT_kb8UMeMFII0wxcA4PXZ5EDAqctA-p1ktCgrtWzxyj0g&oe=6350A6EF",
-      nickName: "Apt47",
-      description: "Special",
-      skill: "security Tester",
-    },
-  ];
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/439248672836943873/a_161fafa0e10d9a57f085e6700e97679e.gif?size=1024",
+        nickName: "DeeKit",
+        description: "Special",
+        skill: "Firewall Man",
+      },
+      {
+        image:
+          "https://cdn.discordapp.com/avatars/714872727600758805/fab141022f777a09b6717f0e19450509.png?size=1024",
+        nickName: "Apt47",
+        description: "Special",
+        skill: "security Tester",
+      },
+    ];
   return { myG, Special };
 };
