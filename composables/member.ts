6b0d2446a5a539aa72b875434bdf7c38b89b72f7
Zensos <rockemaner@gmail.com>
2023-01-19 20:49:25 +0700

    🍕 Pizza Huts

--- composables/member.ts
+++ composables/member.ts
@@ -10,28 +10,28 @@ export const useMember = () => {
           "https://media.discordapp.net/attachments/872102608909795449/1032357736123469874/unknown.png?size=1024",
         nickName: "BossNz",
         description: "Co-Founder",
-        skill: "devops engineer",
+        skill: "Devops Engineer",
       },
       {
         image:
-          "https://cdn.discordapp.com/avatars/502056862674452481/5a712595f474ecdca4fd9141f0fe0e0c.png?size=1024",
+          "https://media.discordapp.net/attachments/872102608909795449/1065626706142052382/5a712595f474ecdca4fd9141f0fe0e0c.png",
         nickName: "Zensos",
         description: "Co-Founder",
-        skill: "Backend Develop",
+        skill: "Devops Engineer",
       },
       {
         image:
-          "https://cdn.discordapp.com/avatars/722467073762000966/36f838836eb638cc1c4efc701a28b1ce.webp?size=1024",
+          "https://media.discordapp.net/attachments/872102608909795449/1065627076570382336/4bd3e8fc3654711b1dbe0747dc8875f4.png?width=676&height=676",
         nickName: "CheLos",
         description: "Co-Founder",
-        skill: "devops engineer",
+        skill: "Devops Engineer",
       },
       {
         image:
-          "https://cdn.discordapp.com/avatars/373412590136721408/0158781b482a75bf929df29e534dd245.webp?size=1024",
+          "https://media.discordapp.net/attachments/872102608909795449/1065627987636146226/0158781b482a75bf929df29e534dd245.png?width=676&height=676",
         nickName: "NackZa",
         description: "Superman",
-        skill: "hackerman",
+        skill: "Hackerman",
       },
 
       {
@@ -43,7 +43,7 @@ export const useMember = () => {
       },
       {
         image:
-          "https://cdn.discordapp.com/avatars/693514816093945888/56afb6223a9b104eb9ae48f9f5141299.webp?size=1024",
+          "https://media.discordapp.net/attachments/872102608909795449/1065628403451039775/5fce63abab5bb17b2ad1b1501c577838.png",
         nickName: "LILDUI",
         description: "Superman",
         skill: "Boxing Fivem",
@@ -52,64 +52,64 @@ export const useMember = () => {
         image:
           "https://cdn.discordapp.com/avatars/572663465718710272/0fdbbe0e17b13f363799fe5b04b8923d.webp?size=1024",
         nickName: "beeb",
-        description: "member",
-        skill: "fullstack devlop",
+        description: "Member",
+        skill: "Fullstack Develop",
       },
       {
         image:
           "https://cdn.discordapp.com/avatars/780633074190123058/2c0014c12c7a1768ebddf3393a980fff.webp?size=1024",
         nickName: "Kimbius",
-        description: "member",
+        description: "Member",
         skill: "Fullstack Develop",
       },
       {
         image:
           "https://cdn.discordapp.com/avatars/328526902279143434/2a2364196177b74b0390a586eb03a027.webp?size=1024",
         nickName: "MAMMOZ",
-        description: "member",
+        description: "Member",
         skill: "Fullstack develop",
       },
       {
         image:
           "https://media.discordapp.net/attachments/943188689952063499/1030488269449875618/unknown.png?size=1024",
         nickName: "Feezial",
-        description: "member",
+        description: "Member",
         skill: "Frontend Develop",
       },
       {
         image:
           "https://cdn.discordapp.com/avatars/809435994394722374/a08f634e16f4d7873a3a1c9165cd5328.webp?size=1024",
         nickName: "safestabi",
-        description: "member",
+        description: "Member",
         skill: "Frontend Develop",
       },
       {
         image:
           "https://cdn.discordapp.com/avatars/930045306261864448/3ac1293446e5790dba50bc18bff5bbea.webp?size=1024",
         nickName: "OhayoOnii",
-        description: "member",
+        description: "Member",
         skill: "Fullstack Develop",
       },
       {
         image:
           "https://cdn.discordapp.com/attachments/959101976149364796/1040593183488344094/unknown.png",
         nickName: "Paotung",
-        description: "member",
+        description: "Member",
         skill: "Fullstack Develop",
       },
       {
         image:
           "https://cdn.discordapp.com/avatars/606422912525926400/d4780a91f587136fa70e0125992f05c8.png?size=1024",
         nickName: "Redz",
-        description: "member",
+        description: "Member",
         skill: "Fullstack Develop",
       },
       {
         image:
-          "https://cdn.discordapp.com/avatars/955587690856849478/f21641bddad6d48aff53e6d9f2e07455.png?size=1024",
+          "https://media.discordapp.net/attachments/872102608909795449/1065627714830221412/a_208d576c5a5f654092efe0df4a800fad.gif",
         nickName: "Larina",
-        description: "member",
-        skill: " HackerMan",
+        description: "Member",
+        skill: "HackerMan",
       }
     ];
   const Special: {
@@ -127,10 +127,10 @@ export const useMember = () => {
       },
       {
         image:
-          "https://cdn.discordapp.com/avatars/714872727600758805/fab141022f777a09b6717f0e19450509.png?size=1024",
+          "https://media.discordapp.net/attachments/872102608909795449/1065627911819907072/b369b87fa5e6ff55eab174b34382192d.png",
         nickName: "Apt47",
         description: "Special",
-        skill: "security Tester",
+        skill: "Security Tester",
       },
     ];
   return { myG, Special };
